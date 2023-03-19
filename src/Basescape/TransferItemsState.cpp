39f04e062a2ee7cafb230993b45f6afc401c34af
FlaubertNHR <flaubertSt@gmail.com>
2023-03-19 22:52:06 +0100

    Basesecape Hangars reworked
    This branch introduces two new features to OXCE+, related to Hangars &
    Crafts management.
    
    First, we can define different hangar types to host crafts.
    
    - A new key ("hangarType") is added for both crafts & facilities
    rulesets (YAML) so the value for the craft must match the value at the
    hangar to be allocated inside.
    
    - If that key is not defined, a default value (-1) is assigned, so both
    crafts and hangar facilities behave as in original OXCE+.
    
    - Also, an "Advanced Option" to show hangar types in crafts' UEFOPEDIA
    entries is added. For Facilitie' UEFOPEDIA entries the modder should
    add the text describing the hangar type.
    
    - Added "hangar Type" un "Stats for Nerds" screen for both crafts and
    facilities.
    
    Second, now is allowed to allocate more than 1 craft in Hangars (as
    long as crafts have the same hangarType key than Hangar facility). All
    crafts are shown in Basescape view inside their hangars, and you can
    right-click an specific craft to go to its "craft screen".
    
    - For hangars with more than 1 craft, a new key ("craftSlots") is added
    to the facilities ruleset (YAML) in order to define so many 'position
     vectors' [x,y,z] as crafts are allowed:
    
        crafts: 2
        craftSlots: # mandatory if crafts > 1; optional otherwise
          - [11, -11, 0]  # Distance [x,y] to the
          - [-11, 11, 0]  # central point
    
    z value is not used.
    
    - If that key is not defined (and crafts=1), a default slot ([0, 0, 0])
    is assigned, so it behaves as in "original" OXCE.
    
    - We have adjusted  "damageFacility", "destroyfacility"&"removeCraft"
    at Base.cpp to consider a vector of crafts instead a single one (so,
     more than one craft and their allocated items and soldiers are
     considered)

--- src/Basescape/TransferItemsState.cpp
+++ src/Basescape/TransferItemsState.cpp
@@ -62,7 +62,7 @@ namespace OpenXcom
  */
 TransferItemsState::TransferItemsState(Base *baseFrom, Base *baseTo, DebriefingState *debriefingState) :
 	_baseFrom(baseFrom), _baseTo(baseTo), _debriefingState(debriefingState),
-	_sel(0), _total(0), _pQty(0), _cQty(0), _aQty(0), _iQty(0.0), _distance(0.0), _ammoColor(0),
+	_sel(0), _total(0), _pQty(0), _aQty(0), _iQty(0.0), _distance(0.0), _ammoColor(0),
 	_previousSort(TransferSortDirection::BY_LIST_ORDER), _currentSort(TransferSortDirection::BY_LIST_ORDER), _errorShown(false)
 {
 	// Create objects
@@ -902,6 +902,7 @@ void TransferItemsState::increaseByValue(int change)
 	RuleItem *selItem = 0;
 	Craft *craft = 0;
 
+	int p = 0;	
 	switch (getRow().type)
 	{
 	case TRANSFER_SOLDIER:
@@ -914,7 +915,8 @@ void TransferItemsState::increaseByValue(int change)
 		break;
 	case TRANSFER_CRAFT:
 		craft = (Craft*)getRow().rule;
-		if (_cQty + 1 > _baseTo->getAvailableHangars() - _baseTo->getUsedHangars())
+		p = craft->getRules()->getHangarType();
+		if (_tCQty[p] + 1 > _baseTo->getAvailableHangars(p) - _baseTo->getUsedHangars(p))	
 		{
 			errorMessage = tr("STR_NO_FREE_HANGARS_FOR_TRANSFER");
 		}
@@ -961,7 +963,7 @@ void TransferItemsState::increaseByValue(int change)
 			_total += getRow().cost * change;
 			break;
 		case TRANSFER_CRAFT:
-			_cQty++;
+			_tCQty[p]++;
 			_pQty += craft->getNumTotalSoldiers();
 			_iQty += craft->getTotalItemStorageSize();
 			getRow().amount++;
@@ -1023,6 +1025,7 @@ void TransferItemsState::decreaseByValue(int change)
 {
 	if (0 >= change || 0 >= getRow().amount) return;
 	Craft *craft = 0;
+	int	p = 0; 
 	change = std::min(getRow().amount, change);
 
 	switch (getRow().type)
@@ -1034,7 +1037,8 @@ void TransferItemsState::decreaseByValue(int change)
 		break;
 	case TRANSFER_CRAFT:
 		craft = (Craft*)getRow().rule;
-		_cQty--;
+		p = craft->getRules()->getHangarType();		
+		_tCQty[p]--;
 		_pQty -= craft->getNumTotalSoldiers();
 		_iQty -= craft->getTotalItemStorageSize();
 		break;
