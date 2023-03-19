1e98ac39f7f44703a3cdd7cf315e7b40982a18c6
FlaubertNHR <flaubertSt@gmail.com>
2023-03-19 22:52:06 +0100

    Rebase to OXCE+ 7.15

--- src/Mod/RuleCraft.cpp
+++ src/Mod/RuleCraft.cpp
@@ -110,6 +110,7 @@ void RuleCraft::load(const YAML::Node &node, Mod *mod, const ModScript &parsers)
 	{
 		_marker = mod->getOffset(node["marker"].as<int>(_marker), 8);
 	}
+	_hangarType = node["hangarType"].as<int>(_hangarType);	
 	_weapons = node["weapons"].as<int>(_weapons);
 	_maxUnitsLimit = node["maxUnitsLimit"].as<int>(_maxUnitsLimit);
 	_pilots = node["pilots"].as<int>(_pilots);
@@ -273,6 +274,16 @@ int RuleCraft::getSprite(int skinIndex) const
 int RuleCraft::getMarker() const
 {
 	return _marker;
+}
+
+/**
+ * Returns the hangar type id in which
+ * craft can be allocated.
+ * @return hangar type ID, -1 if none.
+ */
+int RuleCraft::getHangarType() const	
+{
+	return _hangarType;
 }
 
 /**
