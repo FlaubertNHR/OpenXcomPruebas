91b6d5819b6570688532804931fd6247e5d6e2ae
Flaubert <FlaubertSt@gmail.com>
2023-04-29 19:30:28 +0200

    FilterByCraftV1 Allows filter agents lists by crafts they are allocated. Reordering arrow not fully working yet.

--- src/Basescape/SoldiersState.h
+++ src/Basescape/SoldiersState.h
@@ -43,12 +43,13 @@ private:
 	TextButton *_btnOk, *_btnPsiTraining, *_btnTraining, *_btnMemorial;
 	Window *_window;
 	Text *_txtTitle, *_txtName, *_txtRank, *_txtCraft;
-	ComboBox *_cbxSortBy, *_cbxScreenActions;
+	ComboBox *_cbxSortBy, *_cbxScreenActions, *_cbxFilterCraft;
 	TextList *_lstSoldiers;
 	Base *_base;
 	std::vector<Soldier *> _origSoldierOrder, _filteredListOfSoldiers;
 	std::vector<int> _filteredIndicesOfSoldiers;
 	std::vector<SortFunctor *> _sortFunctors;
+	size_t selectedCraftIndex;
 	getStatFn_t _dynGetter;
 	std::vector<std::string> _availableOptions;
 	size_t _mainOffset;
@@ -61,6 +62,8 @@ public:
 	~SoldiersState();
 	/// Handler for changing the sort by combobox.
 	void cbxSortByChange(Action *action);
+	/// Handler for filtering by craft selected in combobox.
+	void cbxFilterCraftByChange(Action *action);	
 	/// Updates the soldier names.
 	void init() override;
 	/// Handler for clicking the Soldiers reordering button.
