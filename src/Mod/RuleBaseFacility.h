e1397453b70a59fe69dc14082c20bddf85278f54
Flaubert <FlaubertSt@gmail.com>
2024-12-02 18:33:48 +0100

    Some cleaning
    Deleted all stuff related to other features.
    Changed README

--- src/Mod/RuleBaseFacility.h
+++ src/Mod/RuleBaseFacility.h
@@ -74,8 +74,7 @@ private:
 	int _manaRecoveryPerDay = 0;
 	int _healthRecoveryPerDay = 0;
 	float _sickBayAbsoluteBonus, _sickBayRelativeBonus;
-	int _prisonType;
-	int _hangarType;	
+	int _prisonType;	
 	int _rightClickActionType;
 	std::vector<VerticalLevel> _verticalLevels;
 	std::vector<const RuleBaseFacility*> _leavesBehindOnSell;
@@ -83,8 +82,7 @@ private:
 	bool _canBeBuiltOver;
 	bool _upgradeOnly;
 	std::vector<const RuleBaseFacility*> _buildOverFacilities;
-	std::vector<Position> _storageTiles;
-	std::vector<Position> _craftSlots;		
+	std::vector<Position> _storageTiles;	
 	std::string _destroyedFacilityName;
 	const RuleBaseFacility* _destroyedFacility;
 
@@ -217,8 +215,6 @@ public:
 	float getSickBayRelativeBonus() const { return _sickBayRelativeBonus; }
 	/// Gets the prison type.
 	int getPrisonType() const;
-	/// Gets the hangar type.
-	int getHangarType() const;	
 	/// Gets the action type to perform on right click.
 	int getRightClickActionType() const;
 	/// Gets the vertical levels for this facility map generation.
@@ -236,9 +232,7 @@ public:
 	/// Gets which facilities are allowed to be replaced by this building
 	const std::vector<const RuleBaseFacility*> &getBuildOverFacilities() const { return _buildOverFacilities; }
 	/// Gets a list of which tiles are used to place items stored in this facility
-	const std::vector<Position> &getStorageTiles() const;
-	/// Gets a list of relative positions to place crafts over the facility sprite
-	const std::vector<Position> &getCraftSlots() const;				
+	const std::vector<Position> &getStorageTiles() const;			
 	/// Gets the ruleset for the destroyed version of this facility.
 	const RuleBaseFacility* getDestroyedFacility() const;
 };
