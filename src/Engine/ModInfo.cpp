30eab6e3f2b9ba0adadb99f4c1066b441d39f501
FlaubertNHR <flaubertSt@gmail.com>
2025-06-25 08:57:33 +0200

    Changed compatible version

--- src/Engine/ModInfo.cpp
+++ src/Engine/ModInfo.cpp
@@ -181,7 +181,7 @@ struct EngineData
  */
 const EngineData supportedEngines[] = {
 	{ OPENXCOM_VERSION_ENGINE, { OPENXCOM_VERSION_NUMBER }},	
-	{ "Extended", { 8,2,0,0 }},	
+	{ "Extended", { 8,3,1,0 }},	
 	{ "", { 0, 0, 0, 0 } }, // assume that every engine support mods from base game, remove if its not true.
 };
 
